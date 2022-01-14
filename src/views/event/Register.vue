bb80a01fe0608877e449b3039a9492bc46930e1f
Onildo Ferraz <onildo@norteldata.com.br>
2022-01-14 13:54:45 -0300

    Touring Vue Router - Programmatic Navigation pront

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,10 +1,26 @@
 <template>
   <p>Registration form here</p>
+  <button v-on:click="register">Register Meeeeeee</button>
 </template>
 
 <script>
 export default {
   props: ['event'],
+  methods: {
+    register() {
+      // If registration API call is successful,
+      this.$router.replace({
+        name: 'EventDetails',
+        params: { id: this.event.id },
+      })
+      /*
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id },
+      })
+      */
+    },
+  },
 }
 </script>
 
